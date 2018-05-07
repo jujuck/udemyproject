ba3a294497f41c04a7abc2eb0980793a3f44411f
jujuck <julien.y.richard@gmail.com>
2018-05-07 11:06:09 +0200

    mise en forme du style

--- src/App.js
+++ src/App.js
@@ -1,80 +1,137 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
+import Radium, {StyleRoot } from 'radium'
 
 class App extends Component {
     state = {
             persons: [
                 {
+                    id: 'asfa1',
                     name: "Max",
                     age: 28
                 },
                 {
+                    id: 'asfa2',
                     name: "Manu",
                     age: 29
                 },
                 {
+                    id: 'asfa3',
                     name: "Yola",
                     age: 25
                 }
             ],
+            showPersons: false
         }
 
-    
-    switchNameHandler = (newName) => {
-        //console.log('was clicked');
-        this.setState({
-            persons: [
-                {name: "Maximilien", age: 28},
-                {name: "Manuel", age: 29},
-                {name: "Yola", age: 25}
-            ]
-        })
+    //Fonction de changement de nom
+    nameChangeHandler = (event, id) => {
+        //Récupération de l'index de l'object person 
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+        
+        
+        //Récupération et stockage de l'objet person
+        const person = {
+            ...this.state.persons[personIndex]
+        }
+        
+        
+        //Enregistrement du nouveau nom dans l'objet person
+        person.name = event.target.value;
+        
+        
+        //Enregistrement de l'objet person dans le tableau de persons
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+        
+        this.setState({ persons: persons})
     }
     
-    nameChangeHandler = (event) => {
-        this.setState({
-            persons: [
-                {name: "Max", age: 28},
-                {name: event.target.value, age: 29},
-                {name: "Yola", age: 25}
-            ]
-        })
+    
+    //fonction pour effacer les noms
+    deletePersonHanlder = (personIndex) => {
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1)
+        this.setState( {persons: persons})
+        
+    }
+    
+    
+    //Fonction pour affcher les noms
+    togglePersonHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
     }
     
     render() {
+        
+        //définition du style du  bouton en particulier
         const style = {
-            backgroundColor : 'white',
+            backgroundColor : 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
             margin: '5px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover':  {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }//Possible grace à l'intégration de radium
+        };
+        
+        let persons = null;
+        if(this.state.showPersons) {
+           persons = (
+             <div>
+                {this.state.persons.map((person, index) => {
+                    return (
+                        <Person 
+                        click={() => this.deletePersonHanlder(index)}// Appel à la fonction établie dans le fichier persons
+                        name={person.name}
+                        age={person.age} 
+                        key={person.id}//Obligatoire
+                        changed={(event) => this.nameChangeHandler(event, person.id)}/> //Appel de la fonction changed établie dans le fichier persons   
+                    )    
+                })}
+            </div>
+           );
+            //Changement du style en fonction de la réponse 
+            style.backgroundColor ='red';
+            style[':hover']=  {
+                backgroundColor: 'lightred',
+                color: 'black'
+            }//Possible grace à l'intégration de radium
+            
         }
         
+        const classes = [];
+            if (this.state.persons.length <= 2) {
+                classes.push('red')
+            }
+            if (this.state.persons.length <= 1) {
+                classes.push('bold')
+            }
+                
+        
         return (
-            <div className="App">
-                <h1>Hi!!, I'm a react App</h1>
-                <p>This is really working!</p>
-                <button 
-                    style={style}
-                    onClick={this.switchNameHandler}>Switch Name</button>
-                <Person 
-                    name={this.state.persons[0].name} 
-                    age={this.state.persons[0].age} />
-                <Person 
-                    name={this.state.persons[1].name} 
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler}
-                    changed={this.nameChangeHandler}>My Hobbies : Reading
-                </Person>
-                <Person 
-                    name={this.state.persons[2].name} 
-                    age={this.state.persons[2].age} />
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1>Hi!!, I'm a react App</h1>
+                    <p className={classes.join(' ')}>This is really working!</p>
+                    <button 
+                        style={style}
+                        onClick={this.togglePersonHandler}
+                        >Toggle Person
+                    </button>
+                    {persons}   
+                </div>
+            </StyleRoot>
     );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi!!!, I AM A REACT APP'))
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './Person.css';
+//import Radium from 'radium'
 
 
 const Person = (props) => {
+    //Définition du style de l'element
+/*    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }*/
+    
+    
     return (
         <div className="Person">
-            <p>I'm {props.name}, I am {props.age} years old.</p>
+            <p onClick={props.click}>I'm {props.name}, I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     );
 }
 
-export default Person;
+export default /*Radium(*/Person;
